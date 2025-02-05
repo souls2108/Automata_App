5d3211199452708f4cd82dc295b558d1a2a5f09d
souls <115924743+souls2108@users.noreply.github.com>
2025-02-06 00:10:14 +0530

    works using in app web view

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'dart:ffi';
+import 'dart:typed_data';
+import 'dart:convert';
 
+import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:automata_app/plugin/ffi_plugin/automata_lib.dart';
 import 'package:ffi/ffi.dart';
-import 'package:flutter/material.dart';
+
+import 'package:flutter_svg/flutter_svg.dart';
 
 import 'dart:developer' as devtools show log;
 
@@ -12,8 +17,92 @@ void main() {
     theme: ThemeData(
       primarySwatch: Colors.deepPurple,
     ),
-    home: const HomePage(),
+    home: HomePage(),
   ));
+}
+
+class GraphViewer extends StatefulWidget {
+  String dotString;
+
+  GraphViewer({required this.dotString});
+  @override
+  _GraphViewerState createState() => _GraphViewerState();
+}
+
+class _GraphViewerState extends State<GraphViewer> {
+  InAppWebViewController? webViewController;
+  bool _isWebViewLoaded = false;
+  String svgData = '';
+
+  String htmlContent = '''
+  <!DOCTYPE html>
+  <html>
+  <head>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/viz.js/2.1.2/viz.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/viz.js/2.1.2/full.render.js"></script>
+  </head>
+  <body>
+    <div id="graph"></div>
+  </body>
+  </html>
+  ''';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Graph Renderer")),
+      body: Center(
+        child: Column(
+          children: [
+            Expanded(
+              child: InAppWebView(
+                initialData: InAppWebViewInitialData(data: htmlContent),
+                onWebViewCreated: (controller) {
+                  webViewController = controller;
+                },
+                onLoadStop: (controller, url) {
+                  setState(() {
+                    _isWebViewLoaded = true;
+                    devtools.log("WebView loaded!");
+                    generateGraph(widget.dotString);
+                  });
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void generateGraph(String dot) async {
+    if (webViewController == null || !_isWebViewLoaded) {
+      devtools.log("WebView is not ready");
+      return;
+    }
+    devtools.log("Generating graph...");
+    try {
+      String escapedDot = dot.replaceAll('\n', '\\n').replaceAll('"', '\\"');
+      String jsCode = '''
+        (function() {
+          if (typeof Viz !== 'undefined') {
+            console.log("Executing Viz...");
+            let viz = new Viz();
+            viz.renderSVGElement("$escapedDot")
+                .then(function(element) {
+                    document.getElementById('graph').appendChild(element);
+                })
+          } else {
+            console.log("Error: Viz.js not loaded");
+            return "Error: Viz.js not loaded";
+          }
+        })();
+      ''';
+      await webViewController!.evaluateJavascript(source: jsCode);
+    } catch (e) {
+      devtools.log("Error evaluating JavaScript: $e");
+    }
+  }
 }
 
 class HomePage extends StatefulWidget {
@@ -57,22 +146,28 @@ class _HomePageState extends State<HomePage> {
           ),
           ElevatedButton(
             onPressed: () {
-              devtools.log(_regex.text);
               final regexPointer = _regex.text.toNativeUtf8().cast<Char>();
               final dfa =
                   AutomataLib().nativeLibrary.DFA_create_instance(regexPointer);
+              final mdfa = AutomataLib().nativeLibrary.DFA_minimalDFA(dfa);
               malloc.free(regexPointer);
 
               setState(() {
                 final outputPointer =
-                    AutomataLib().nativeLibrary.DFA_generateDotText(dfa);
+                    AutomataLib().nativeLibrary.DFA_generateDotText(mdfa);
                 _output = outputPointer.cast<Utf8>().toDartString();
                 malloc.free(outputPointer);
               });
+
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => GraphViewer(dotString: _output),
+                ),
+              );
             },
             child: const Text('Match'),
           ),
-          Text("OUTPUT: $_output"),
         ],
       ),
     );
